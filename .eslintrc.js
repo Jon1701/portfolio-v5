85c2b3890f72cb17e71ce2eaeec9a42ecd52e421
Jon Balon <development@jonbalon.com>
2021-12-10 21:15:10 -0500

    ESLint Configuration File
    - extend config shipped with Gatsby
    - ignore `node_modules` folder

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    globals: {
+      __PATH_PREFIX__: true,
+    },
+    extends: `react-app`, // Extends the config shipped with Gatsby.
+    "ignorePatterns": ["node_modules/", ".cache", "public"]
+  }
