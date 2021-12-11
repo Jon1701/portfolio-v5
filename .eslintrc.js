db4d883b1c993eca99f70fd7a536e8d834ca4935
Jon Balon <development@jonbalon.com>
2021-12-10 22:57:42 -0500

    Update ESLint Configuration File to use Prettier Ruleset

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,9 @@ module.exports = {
     globals: {
       __PATH_PREFIX__: true,
     },
-    extends: `react-app`, // Extends the config shipped with Gatsby.
+    extends: [
+      'react-app', // Extends the config shipped with Gatsby.
+      'prettier',
+    ], 
     "ignorePatterns": ["node_modules/", ".cache", "public"]
   }
