4da73c1596d65ef0069c7279316c80bba0f417df
Jon Balon <development@jonbalon.com>
2021-12-21 20:55:21 -0500

    Define CSS Breakpoints for Mobile, Tablet, and Desktop Screen Sizes

---
+++ src/util/responsiveDesign/breakpoints.js
@@ -0,0 +1,18 @@
+/**
+ * Defines the breakpoints for mobile, tablet, and desktop screens.
+ */
+const breakpoints = Object.freeze({
+  mobile: {
+    min: 320,
+    max: 767,
+  },
+  tablet: {
+    min: 768,
+    max: 1023,
+  },
+  desktop: {
+    min: 1024,
+  },
+});
+
+export default breakpoints;
