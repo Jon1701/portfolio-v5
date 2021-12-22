d493646806cb73ce5b2d4ceb0fdce72443aab9f0
Jon Balon <development@jonbalon.com>
2021-12-21 20:21:33 -0500

    404 Page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,50 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import styled from 'styled-components';
+import { Helmet } from 'react-helmet';
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+import GlobalStyle from '_styles/Global';
+import InternalLinkButton from '_components/Buttons/Button/InternalLink';
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+/**
+ * Component container.
+ */
+const Container = styled.main`
+  background-color: #232023;
+  color: #fff;
+  min-height: 100vh;
 
-// markup
-const NotFoundPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+`;
 
-export default NotFoundPage
+/**
+ * Custom H1 component.
+ */
+const H1 = styled.h1`
+  margin-top: 0;
+`;
+
+/**
+ * 404 Not Found page.
+ *
+ * @returns {React.Component} 404 Not Found page.
+ */
+const NotFoundPage = () => (
+  <Container>
+    <Helmet>
+      <title>Page not found</title>
+    </Helmet>
+
+    <GlobalStyle />
+
+    <H1>404</H1>
+
+    <p>The page you are looking for does not exist.</p>
+
+    <InternalLinkButton to="/">Go Back to the Home Page</InternalLinkButton>
+  </Container>
+);
+
+export default NotFoundPage;
