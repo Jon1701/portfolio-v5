bb0720a80ca305d6c67c3c7ac65a09d5100d219e
Jon Balon <development@jonbalon.com>
2021-12-21 19:08:14 -0500

    Add `hideUnderline` prop to the ExternalLink component

--- src/components/Links/ExternalLink/index.js
+++ src/components/Links/ExternalLink/index.js
@@ -10,6 +10,8 @@ const CustomLink = styled.a`
   color: inherit;
 
   cursor: pointer;
+
+  text-decoration: ${props => (props.hideUnderline ? 'none' : 'underline')};
 `;
 
 /**
@@ -17,18 +19,28 @@ const CustomLink = styled.a`
  *
  * @param {object} props Component props.
  * @param {string} props.href Link destination.
+ * @param {boolean} props.hideUnderline Hides the underline text decoration.
  * @param {React.Component} props.children Child nodes to render.
  * @returns {React.Component} External link.
  */
-const ExternalLink = ({ href, children }) => (
-  <CustomLink href={href} target="_blank" rel="noreferrer noopener">
+const ExternalLink = ({ href, hideUnderline, children }) => (
+  <CustomLink
+    href={href}
+    target="_blank"
+    rel="noreferrer noopener"
+    hideUnderline={hideUnderline}>
     {children}
   </CustomLink>
 );
 
 ExternalLink.propTypes = {
   href: PropTypes.string.isRequired,
+  hideUnderline: PropTypes.bool,
   children: PropTypes.node.isRequired,
+};
+
+ExternalLink.defaultProps = {
+  hideUnderline: false,
 };
 
 export default ExternalLink;
