452c14643d39a7be71061ae2c5051a9166aa3772
Jon Balon <development@jonbalon.com>
2021-12-21 23:17:25 -0500

    Project Details Page Template uses Responsive Design

--- src/templates/Pages/Project/index.js
+++ src/templates/Pages/Project/index.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 import HIXSEvolutionPattern from '_images/patterns/hixs_pattern_evolution.png';
+import desktopBreakpoint from '_util/responsiveDesign/desktopBreakpoint';
+import mobileBreakpoint from '_util/responsiveDesign/mobileBreakpoint';
+import tabletBreakpoint from '_util/responsiveDesign/tabletBreakpoint';
 
 /**
  * Page template.
@@ -8,20 +11,29 @@ import HIXSEvolutionPattern from '_images/patterns/hixs_pattern_evolution.png';
 const Page = styled.main`
   background-image: url(${HIXSEvolutionPattern});
   background-attachment: fixed;
+  padding-top: 35px;
 
   color: #fff;
-  min-height: 100vh;
 `;
 
 /**
  * Section margin.
  */
 const Margin = styled.div`
-  max-width: 800px;
-  margin: 0 auto;
-  padding: 50px 0;
+  ${mobileBreakpoint`
+    margin-left: 10px;
+    margin-right: 10px;
+  `}
 
-  width: 100%;
+  ${tabletBreakpoint`
+    margin-left: 50px;
+    margin-right: 50px;
+  `}
+
+  ${desktopBreakpoint`
+    max-width: 900px;
+    margin: 0 auto;
+  `}
 `;
 
 export { Page, Margin };
