5d4558d0377e959274f4708045840fc1f4bf11db
Jon Balon <development@jonbalon.com>
2021-12-23 00:15:39 -0500

    Link to Return to Projects Section on Home page

--- src/templates/Pages/Project/index.js
+++ src/templates/Pages/Project/index.js
@@ -1,7 +1,9 @@
+import React from 'react';
 import styled from 'styled-components';
 
-import HIXSEvolutionPattern from '_images/patterns/hixs_pattern_evolution.png';
 import desktopBreakpoint from '_util/responsiveDesign/desktopBreakpoint';
+import HIXSEvolutionPattern from '_images/patterns/hixs_pattern_evolution.png';
+import InternalLink from '_components/Links/InternalLink';
 import mobileBreakpoint from '_util/responsiveDesign/mobileBreakpoint';
 import tabletBreakpoint from '_util/responsiveDesign/tabletBreakpoint';
 
@@ -37,4 +39,15 @@ const Margin = styled.div`
   `}
 `;
 
-export { Page, Margin };
+/**
+ * Link which returns the user to the Projects section on the home page.
+ *
+ * @returns {React.Component} Link.
+ */
+const ReturnToProjectsSectionLink = () => (
+  <div style={{ marginTop: '100px', textAlign: 'center' }}>
+    <InternalLink to="/#projects">&larr; Return to Projects</InternalLink>
+  </div>
+);
+
+export { Page, Margin, ReturnToProjectsSectionLink };
